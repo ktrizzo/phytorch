02d6884ef6cd933ceb286020892ebc5066e5228e
ktrizzo <95317969+ktrizzo@users.noreply.github.com>
2025-11-05 21:18:55 -0800

    Deploy website - based on 17667971b72e92be10e5bdb660335653d2d75d15

--- website/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/kylerizzo/Garden/code/phytorch/website/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/kylerizzo/Garden/code/phytorch/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/kylerizzo/Garden/code/phytorch/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/kylerizzo/Garden/code/phytorch/website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/kylerizzo/Garden/code/phytorch/website/src/css/custom.css"),
-];
